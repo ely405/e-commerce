e3f90744728f53a2356737eea9dfe84240e1eac9
ely405 <gonzalesm405@gmail.com>
2018-04-17 10:34:26 -0500

    instalando dependecias para tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import App from './App';
 
+import { shallow } from "enzyme";
+/*
+  * shallow --> prueba el componente de forma aislada, sin renderizar los subcomponentes que tenga,
+    ni ejecuta los métodos de ciclo de vida
+  * render --> renderiza los componentes a HTML stático, incluye los subcomponentes
+    y analiza la estructura HTML
+  * mount --> utiliza el JSDOM, para renderizsr los subcomponentes, probar la interacción
+    con API's del DOM o probar el ciclo de vida completo del componente
+*/
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
